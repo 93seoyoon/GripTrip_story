7decf1e52231965fa858444ab2d47a566e2d89f9
KOSTA-HP\KOSTA <hoon2155@gmail.com>
2017-06-23 15:24:42 +0900

    insertStoryInfo, updateStory, upStoryHit sql 추가

---
+++ src/test/java/spring/trip/test/StoryTest.java
@@ -0,0 +1,18 @@
+package spring.trip.test;
+
+import java.io.Reader;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+
+public class StoryTest {
+
+		  @Test
+		   public void jTest() throws Exception{
+		  
+		  }
+}
